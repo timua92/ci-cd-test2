5e474c4af45a8ccf7633ee0d255d1fd0d799fa6e
timua92 <tim.edwards@ua92.ac.uk>
2025-09-16 09:42:51 +0000

    feat: update homepage message

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello, CI/CD! My project is ready!');
+  res.send('This is my updated text!');
 });
 
 module.exports = app;
